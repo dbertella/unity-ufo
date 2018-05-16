34fb37fd301e14033b61937f8d74a812ea5ff0b0
dbertella <daniele.bertella@gmail.com>
2018-05-17 00:45:20 +0100

    Ufo game, not funny at all but we are starting it

---
+++ Assets/Scripts/PickupsPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupsPool : MonoBehaviour {
+
+	public int pickupPoolSize = 12;
+	public GameObject pickup;
+	public float min;
+	public float max;
+
+	private GameObject[] pickups;
+
+	// Use this for initialization
+	void Start () {
+		pickups = new GameObject[pickupPoolSize];
+		for (int i = 0; i < pickupPoolSize; i++) {
+			float spawnYPosition = Random.Range (min, max);
+			float spawnXPosition = Random.Range (min, max);
+			Vector2 objectPoolPosition = new Vector2 (spawnYPosition, spawnXPosition);
+			pickups [i] = (GameObject)Instantiate (pickup, objectPoolPosition, Quaternion.identity);
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	public Text niceTime;
+
+	private DateTime startTime;
+	private int count;
+	private Rigidbody2D rb2d;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		count = 0;
+		startTime = DateTime.Now;
+		winText.text = "";
+		niceTime.text = "";
+		setCountText ();
+	}
+	
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+		Timer_Tick ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText() {
+		countText.text = string.Format("Count: {0}", count.ToString ());
+
+		if (count >= 12) {
+			winText.text = string.Format("You win: {0}", niceTime.text);
+		}
+	}
+
+	private void Timer_Tick() {
+		if (count < 12) {
+			TimeSpan timeElapsed = DateTime.Now - startTime;
+			niceTime.text = timeElapsed.TotalSeconds.ToString ("0.000");
+		}
+	}
+}
